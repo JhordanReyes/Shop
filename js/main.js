badc421fc7d641f31b3e7e7a2d013b3370dddd06
JhordanReyes <reyesjhordan4@gmail.com>
2022-10-06 15:19:17 -0500

    Agregando animaciones y efectos de scroll

--- js/main.js
+++ js/main.js
@@ -7,10 +7,27 @@
 /*=============== SCROLL SECTIONS ACTIVE LINK ===============*/
 window.sr = ScrollReveal();
 
-sr.reveal(".scroll", {
-    duration: 3000,
-    origin: "left",
+sr.reveal(".header__description", {
+    duration: 2000,
+    origin: "top",
     distance: "100px",
+})
+sr.reveal(".header__images", {
+    duration: 2000,
+    origin: "bottom",
+    distance: "100px",
+})
+
+sr.reveal(".new__img", {
+    duration: 2000,
+    origin: "bottom",
+    distance: "100px",
+})
+
+window.addEventListener("scroll", () => {
+    const nav = document.getElementById("nav");
+    this.scrollY >= 50 ? nav.classList.add("nav-scroll")
+                    : nav.classList.remove("nav-scroll");
 })
 
 /*=============== SHOW SCROLL UP ===============*/ 
