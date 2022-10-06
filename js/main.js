99e8d313636bce7dc9bf9aeccb01a4bba12323db
JhordanReyes <reyesjhordan4@gmail.com>
2022-10-06 08:59:39 -0500

    Agregando nav y el header

---
+++ js/main.js
@@ -0,0 +1,22 @@
+/*=============== CHANGE BACKGROUND HEADER ===============*/
+
+
+/*=============== SWIPER PRODUCTS ===============*/
+
+
+/*=============== SCROLL SECTIONS ACTIVE LINK ===============*/
+window.sr = ScrollReveal();
+
+sr.reveal(".scroll", {
+    duration: 3000,
+    origin: "left",
+    distance: "100px",
+})
+
+/*=============== SHOW SCROLL UP ===============*/ 
+
+
+/*=============== DARK LIGHT THEME ===============*/ 
+
+
+/*=============== SCROLL REVEAL ANIMATION ===============*/
